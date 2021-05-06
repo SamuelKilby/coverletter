a448ab14a85ee9e429e1223cb7a623c06a584ad1
SamuelKilby <80701618+SamuelKilby@users.noreply.github.com>
2021-05-06 09:03:23 -0400

    updated and more efficient version

--- CoverLetter.java
+++ CoverLetter.java
@@ -1,54 +1,105 @@
 import java.util.Scanner;
-public class CoverLetter {
-Scanner scan = new Scanner(System.in);
-String job;
-String company;
-String xp1;
-String xp2;
-String xp3;
-String fields;
-String compliment;
 
+public class CoverLetter {
 
 public static void main(String[] args) {
-		CoverLetter cov = new CoverLetter();
+		String job;
+		String company;
+		String xp;
+		String fields;
+		String x;
+		String y;
+		String type;
+		String compliment = new String("");
+		Scanner scan = new Scanner(System.in);
+
 		System.out.println("Job Title");
-		cov.job = cov.scan.nextLine();
+		job = scan.nextLine();
 		
 		System.out.println("Company Name");
-		cov.company = cov.scan.nextLine();
+		company = scan.nextLine();
+
+		System.out.println("Staffing company? y/n");
+		y = scan.nextLine();
+		if (y == "n")
+		{
+			System.out.println("fields/industries company covers");
+			fields = scan.nextLine();
+			type = " through ";
+		}
+		else
+		{
+			System.out.println("General compliment regarding company");
+			compliment = scan.nextLine();
+			type = " at ";
+		}
 		
-		System.out.println("I have experience with ? from ?");
-		cov.xp1 = cov.scan.nextLine();
+
+		System.out.println("a = agile and scrum\nb = java\nc = SQL and MySQL\n"
+		+ "d = HTML, CSS, and Bootstrap\ne = object-oriented, data stru, algo\n"
+		+ "f = Git");
+		while (x != "x")
+		{
+			x = scan.nextLine();
+			switch (x) {
+			case x = "a":
+				xp.append("I have experience with Agile and Scrum methodologies from my "
+				+ "software engineering and capstone courses at UNCC. ");
+				break;
+			case x = "b":
+				xp.append("I have the most experience with Java from my introduction to"
+				+ " computer science 1 & 2, data structures and algorithms, and discrete"
+				+ " structures courses at UNCC as well as my own personal uses, like a"
+				+ " a cover letter generator. ");
+				break;
+			case x = "c":
+				xp.append("My database implementation and design course gave me experience"
+				+ " with SQL and MySQL. ");
+				break;
+			case x = "d":
+				xp.append("I have hands-on experience with HTML, CSS, and Bootstrap from"
+				+ " my software engineering and capstone courses at UNCC. ");
+				break;
+			case x = "e":
+				xp.append("Over the course of my degree program I learned object-oriented"
+				+ " programming, data structures, algorithms, and coding efficiency. ");
+				break;
+			case x = "f":
+				xp.append("I have had courses that utilize Git and I have used Git for my"
+				+ " own uses. ");
+				break;
+			default:
+				break;
+			}
+		}
 		
-		System.out.println("I also have worked with ? in ?");
-		cov.xp2 = cov.scan.nextLine();
+		String cover = new String("Dear Hiring Manager,\n\nThank you for this opportunity"
+		+ " of " + job + type + company + ". I am a recent graduate from the University"
+		+ " of North Carolina at Charlotte. I have a bachelor's of science in computer"
+		+ " science with a concentration in software, systems, and networking. I believe"
+		+ " I have experience for this position.\n" + xp + "\nI appreciate this opportunity"
+		+ type + company + ". I know I could learn in this position, sharpen skills, and"
+		+ " gain new skills. For that alone, I am highly interested. " + compliment
+		+ "Please contact me if you have any questions or concerns.\n\nGod bless,\nSamuel Kilby");
 		
-		System.out.println("my time in ? ? ? (experience)");
-		cov.xp3 = cov.scan.nextLine();
 		
-		System.out.println("fields/industries company covers");
-		cov.fields = cov.scan.nextLine();
 		
-		System.out.println("General compliment regarding company");
-		cov.compliment = cov.scan.nextLine();
-		
-		String cover = "Dear Hiring Manager, \n\n"
-		+ "Thank you for this opportunity to be a "
-		+ cov.job + " at " + cov.company + ". I am a recent graduate from"
-		+ " the University of North Carolina at Charlotte."
-		+ " I have a bachelor's degree in computer science "
-		+ "from their computer science department with a "
-		+ "concentration in software, systems, and networking."
-			+ " I believe I have experience for this position."
-		+ "\nI have experience with " + cov.xp1 + ". I also have "
-				+ "worked with " + cov.xp2 + ". Through my time in "
-				+ cov.xp3 + ".\n" + cov.company + " seems to be an upstanding"
-				+ " company for which I would like to work. I admire "
-				+ "the reach the company has in the " + cov.fields + 
-				" fields in the industry. " + cov.compliment + ". Please "
-				+ "contact me if you have any questions or concerns."
-				+ "\n \nGod bless,\nSamuel Kilby";
+		// String cover = "Dear Hiring Manager, \n\n"
+		// + "Thank you for this opportunity of "
+		// + job + " at " + company + ". I am a recent graduate from"
+		// + " the University of North Carolina at Charlotte."
+		// + " I have a bachelor's degree in computer science "
+		// + "from their computer science department with a "
+		// + "concentration in software, systems, and networking."
+		// 	+ " I believe I have experience for this position."
+		// + "\nI have experience with " + xp1 + ". I also have "
+		// 		+ "worked with " + xp2 + ". Through my time in "
+		// 		+ xp3 + ".\n" + company + " seems to be an upstanding"
+		// 		+ " company for which I would like to work. I admire "
+		// 		+ "the reach the company has in the " + fields + 
+		// 		" fields in the industry. " + compliment + ". Please "
+		// 		+ "contact me if you have any questions or concerns."
+		// 		+ "\n \nGod bless,\nSamuel Kilby";
 		
 		System.out.println(cover);
 	}
